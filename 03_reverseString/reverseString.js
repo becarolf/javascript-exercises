f0b6b7c68634044becc93e70d5ab7952eae8bfca
Beatriz <becarolf@gmail.com>
2024-05-22 17:12:35 -0400

    Solved exercise 03 by creating a function that return the input reversed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  const arrayStr = str.split('');
+  const reverseArray = arrayStr.reverse();
+  const joinArray = reverseArray.join('');
 
+  return joinArray;
 };
+
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;
